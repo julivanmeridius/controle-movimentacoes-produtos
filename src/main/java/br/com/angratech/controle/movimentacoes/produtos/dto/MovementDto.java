3e5bb42354976ff6ec946ecbcfe7169bbde84705
Julivan Silva <techsolaris@gmail.com>
2022-10-13 11:39:24 -0300

    Criacao DTO

---
+++ src/main/java/br/com/angratech/controle/movimentacoes/produtos/dto/MovementDto.java
@@ -0,0 +1,16 @@
+package br.com.angratech.controle.movimentacoes.produtos.dto;
+
+import java.io.Serializable;
+
+/**
+ * DTO 
+ * @author Julivan Silva 
+ */
+public class MovementDto implements Serializable {
+
+	/** UID **/
+	private static final long serialVersionUID = 7817381985793870222L;
+
+	
+	
+}
