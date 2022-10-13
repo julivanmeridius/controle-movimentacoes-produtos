a87b8c45b906e742bc2891f6fdec37f2aae31dfe
Julivan Silva <techsolaris@gmail.com>
2022-10-13 11:30:55 -0300

    Inicio de configuracoes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into produto (cod_produto, des_produto, sta_status) values ('1010', 'Produto 1010', 1);
+insert into produto (cod_produto, des_produto, sta_status) values ('2020', 'Produto 2020', 1);
+insert into produto (cod_produto, des_produto, sta_status) values ('3030', 'Produto 3030', 1);
+
+insert into produto_cosif (cod_produto, cod_cosif, cod_classificacao, sta_status) values ('1010', 'abcd1', 'Cl 1.1', 1);
+insert into produto_cosif (cod_produto, cod_cosif, cod_classificacao, sta_status) values ('1010', 'abcd2', 'Cl 1.2', 1);
+insert into produto_cosif (cod_produto, cod_cosif, cod_classificacao, sta_status) values ('2020', 'bcde1', 'Cl 2.1', 1);
+insert into produto_cosif (cod_produto, cod_cosif, cod_classificacao, sta_status) values ('2020', 'bcde2', 'Cl 2.2', 1);
+insert into produto_cosif (cod_produto, cod_cosif, cod_classificacao, sta_status) values ('3030', 'bcde2', 'Cl 2.2', 1);
